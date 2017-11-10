6ae87fad87b1c8ff7240652a0c6c053075781657
Tim Otten <totten@civicrm.org>
2017-11-10 00:58:51 -0800

    Autogenerate my.cnf

--- default.nix
+++ default.nix
@@ -11,10 +11,13 @@ stdenv.mkDerivation rec {
   ];
 
   shellHook = ''
-    export BKCTL=JoP1GUxV
+    export BKNXAUTH=JoP1GUxV
     export PATH="$PWD/bin:$PATH"
     export AMPHOME=$PWD/var/amp
-    export MYSQL_HOME=$PWD/etc/mysql
+    export MYSQL_HOME=$PWD/var/mysql/conf
+    alias ls='ls --color=auto'
+    alias lsc='ls --color=auto -F'
+    alias lsx='ls --color=auto -lF'
     bkctl init
   '';
 }
