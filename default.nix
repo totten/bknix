0f6a99afb1d2a5538a9009332003840d8c3b1c00
Tim Otten <totten@civicrm.org>
2018-08-11 17:25:41 -0700

    Add apcu

--- default.nix
+++ default.nix
@@ -13,6 +13,7 @@ let
     { options = ''
             zend_extension=${pkgs.php56Packages.xdebug}/lib/php/extensions/xdebug.so
             extension=${pkgs.php56Packages.redis}/lib/php/extensions/redis.so
+            extension=${pkgs.php56Packages.apcu}/lib/php/extensions/apcu.so
             extension=${pkgs.php56Packages.memcache}/lib/php/extensions/memcache.so
             extension=${pkgs.php56Packages.memcached}/lib/php/extensions/memcached.so
             extension=${pkgs.php56Packages.imagick}/lib/php/extensions/imagick.so
@@ -28,7 +29,7 @@ let
     # add all extensions needed as buildInputs and don't forget to load them in the php.ini above
     phpOverride = stdenv.mkDerivation rec {
         name = "bknix-php-override";
-        buildInputs = [pkgs.php56 pkgs.php56Packages.xdebug pkgs.php56Packages.redis pkgs.php56Packages.memcached pkgs.php56Packages.memcache pkgs.php56Packages.imagick pkgs.makeWrapper];
+        buildInputs = [pkgs.php56 pkgs.php56Packages.xdebug pkgs.php56Packages.redis pkgs.php56Packages.apcu pkgs.php56Packages.memcached pkgs.php56Packages.memcache pkgs.php56Packages.imagick pkgs.makeWrapper];
         buildCommand = ''
           makeWrapper ${pkgs.php56}/bin/php $out/bin/php --add-flags -c --add-flags "${phpIni}"
           makeWrapper ${pkgs.php56}/bin/php-fpm $out/bin/php-fpm --add-flags -c --add-flags "${phpIni}"
