c2b9bfb2d835288816f06e7533bd75341cffc789
Tim Otten <totten@civicrm.org>
2018-09-10 18:20:07 -0400

    restart-ci.sh - Add script to restart all services

---
+++ bin/restart-ci.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "Stopping all services"
+systemctl stop bknix-{dfl,min,max}{,-mysqld} mnt-mysql-jenkins.mount
+
+echo "Waiting"
+# Don't know if this is actually needed, but it's improved reliability in the past.
+sleep 5
+
+echo "Starting all services"
+systemctl start bknix-{dfl,min,max}{,-mysqld} mnt-mysql-jenkins.mount
