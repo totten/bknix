d451f1bd039b1534e2218f579533c20fd4db144e
Tim Otten <totten@civicrm.org>
2018-09-10 19:33:27 -0700

    Add bkpkgs.mysql55, which enables ARCHIVE and other engines

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,7 +1,10 @@
 /**
  * Provide a list of packages which we have defined or customized locally.
+ *
+ * TODO: Consider using callPackages convention, e.g. http://lethalman.blogspot.com/2014/09/nix-pill-13-callpackage-design-pattern.html
  */
 rec {
+   mysql55 = (import ./mysql55/default.nix).mysql55;
    /* NOT WORKING: php55 = import ./php55/default.nix; */
    php56 = import ./php56/default.nix;
    php70 = import ./php70/default.nix;
