cb63068d78dd9ef66dc49ba25ef25380dacc1cd7
Tim Otten <totten@civicrm.org>
2018-09-20 21:28:23 -0700

    install-ci.sh - Create *two* users, `jenkins` and `publisher`

--- bin/install-ci.sh
+++ bin/install-ci.sh
@@ -23,10 +23,51 @@
 #    eval $(use-bknix max)
 #    eval $(use-bknix dfl)
 
-OWNER=${OWNER:-jenkins}
-RAMDISK="/mnt/mysql/$OWNER"
-RAMDISKSVC=$(systemd-escape "mnt/mysql/$OWNER")
-RAMDISKSIZE=8G
+###########################################################
+## Primary install routines
+
+## Setup all services for user "jenkins"
+function install_all_jenkins() {
+  OWNER=jenkins
+  RAMDISK="/mnt/mysql/$OWNER"
+  RAMDISKSVC=$(systemd-escape "mnt/mysql/$OWNER")
+  RAMDISKSIZE=8G
+
+  install_user
+  install_ramdisk
+
+  PROFILE=dfl HTTPD_DOMAIN=$(hostname -f) HTTPD_PORT=8001 MEMCACHED_PORT=12221 PHPFPM_PORT=9009 REDIS_PORT=6380 MYSQLD_PORT=3307 install_profile
+  PROFILE=min HTTPD_DOMAIN=$(hostname -f) HTTPD_PORT=8002 MEMCACHED_PORT=12222 PHPFPM_PORT=9010 REDIS_PORT=6381 MYSQLD_PORT=3308 install_profile
+  PROFILE=max HTTPD_DOMAIN=$(hostname -f) HTTPD_PORT=8003 MEMCACHED_PORT=12223 PHPFPM_PORT=9011 REDIS_PORT=6382 MYSQLD_PORT=3309 install_profile
+
+  unset OWNER RAMDISK RAMDISKSVC RAMDISKSIZE
+}
+
+## Setup all services for user "publisher"
+function install_all_publisher() {
+  OWNER=publisher
+  RAMDISK="/mnt/mysql/$OWNER"
+  RAMDISKSVC=$(systemd-escape "mnt/mysql/$OWNER")
+  RAMDISKSIZE=500M
+
+  install_user
+  install_ramdisk
+
+  PROFILE=dfl HTTPD_DOMAIN=$(hostname -f) HTTPD_PORT=8004 MEMCACHED_PORT=12224 PHPFPM_PORT=9012 REDIS_PORT=6383 MYSQLD_PORT=3310 install_profile
+
+  unset OWNER RAMDISK RAMDISKSVC RAMDISKSIZE
+}
+
+###########################################################
+## Install helpers
+
+function install_user() {
+  if id "$OWNER" 2>/dev/null 1>/dev/null ; then
+    echo "User $OWNER already exists"
+  else
+    adduser --disabled-password "$OWNER"
+  fi
+}
 
 function install_ramdisk() {
   echo "Creating systemd ramdisk \"$RAMDISK\" ($RAMDISKSVC)"
@@ -71,7 +112,12 @@ function template_render() {
     | sed "s/%%PROFILE%%/$PROFILE/g"
 }
 
-########################################
+function install_use_bknix() {
+  echo "Installing global helper \"use-bknix\""
+  cp -f bin/use-bknix /usr/local/bin/use-bknix
+}
+
+###########################################################
 ## Main
 
 if [ -z `which nix` ]; then
@@ -79,17 +125,6 @@ if [ -z `which nix` ]; then
   exit 2
 fi
 
-if id "$OWNER" 2>/dev/null 1>/dev/null ; then
-  echo "User $OWNER already exists"
-else
-  adduser --disabled-password "$OWNER"
-fi
-
-echo "Installing global helper \"use-bknix\""
-cp -f bin/use-bknix /usr/local/bin/use-bknix
-
-install_ramdisk
-
-PROFILE=dfl HTTPD_DOMAIN=$(hostname -f) HTTPD_PORT=8001 MEMCACHED_PORT=12221 PHPFPM_PORT=9009 REDIS_PORT=6380 MYSQLD_PORT=3307 install_profile
-PROFILE=min HTTPD_DOMAIN=$(hostname -f) HTTPD_PORT=8002 MEMCACHED_PORT=12222 PHPFPM_PORT=9010 REDIS_PORT=6381 MYSQLD_PORT=3308 install_profile
-PROFILE=max HTTPD_DOMAIN=$(hostname -f) HTTPD_PORT=8003 MEMCACHED_PORT=12223 PHPFPM_PORT=9011 REDIS_PORT=6382 MYSQLD_PORT=3309 install_profile
+install_use_bknix
+install_all_jenkins
+install_all_publisher
