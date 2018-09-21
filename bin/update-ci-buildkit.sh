4037161c3db060c11a73e48fe151d1dee8bffcf1
Tim Otten <totten@civicrm.org>
2018-09-21 06:34:44 +0000

    update-ci-buildkit.sh - Make it actually work with multiple users

--- bin/update-ci-buildkit.sh
+++ bin/update-ci-buildkit.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 for OWNER in jenkins publisher ; do
   for PROF in dfl min max ; do
-    if [ -f "/home/$OWNER/bknix-$PROF" ]; then
+    if [ -d "/home/$OWNER/bknix-$PROF" ]; then
       echo "Update \"$PROF\" for user \"$OWNER\""
-      su - $OWNER -c 'eval $(use-bknix $PROF) && bknix update'
+      su - $OWNER -c 'eval $(use-bknix '$PROF') && bknix update'
     fi
   done
 done
