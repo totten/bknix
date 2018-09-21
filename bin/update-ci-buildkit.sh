d761f68eb20a5673fe603f2fdffa38951c0da722
Tim Otten <totten@civicrm.org>
2018-09-20 21:28:42 -0700

    update-ci-buildkit.sh - Handle both users (jenkins and publisher)

--- bin/update-ci-buildkit.sh
+++ bin/update-ci-buildkit.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-OWNER=${OWNER:-jenkins}
-su - $OWNER -c 'for PROF in dfl min max ; do eval $(use-bknix $PROF) ; bknix update ; done'
+for OWNER in jenkins publisher ; do
+  for PROF in dfl min max ; do
+    if [ -f "/home/$OWNER/bknix-$PROF" ]; then
+      echo "Update \"$PROF\" for user \"$OWNER\""
+      su - $OWNER -c 'eval $(use-bknix $PROF) && bknix update'
+    fi
+  done
+done
