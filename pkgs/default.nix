504681ef99c8bbaf9808dd25c44c40da4cd922bf
Tim Otten <totten@civicrm.org>
2018-12-04 15:22:22 -0800

    Re-enable php72 pkg

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -9,6 +9,6 @@ rec {
    php56 = import ./php56/default.nix;
    php70 = import ./php70/default.nix;
 #   php71 = import ./php71/default.nix;
-#   php72 = import ./php72/default.nix;
+   php72 = import ./php72/default.nix;
    launcher = import ./launcher;
 }
