ceb37b007062a46504b25352b53935995276f264
Tim Otten <totten@civicrm.org>
2018-12-04 15:24:25 -0800

    Add support for "edge" to mgmt scripts (where support is optional)

--- bin/update-ci-buildkit.sh
+++ bin/update-ci-buildkit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for OWNER in jenkins publisher ; do
-  for PROF in dfl min max ; do
+  for PROF in dfl min max edge ; do
     if [ -d "/home/$OWNER/bknix-$PROF" ]; then
       echo "Update \"$PROF\" for user \"$OWNER\""
       su - $OWNER -c 'eval $(use-bknix '$PROF') && bknix update'
