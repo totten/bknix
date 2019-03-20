54a47687f972aa600d5d56ab74abba82ef87e404
Tim Otten <totten@civicrm.org>
2019-03-20 13:02:35 -0700

    Update loco

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -20,6 +20,6 @@ in rec {
    php72 = import ./php72/default.nix;
    transifexClient = import ./transifexClient/default.nix;
    ramdisk = callPackage (fetchTarball https://github.com/totten/ramdisk/archive/7a24b2d9ca0b64bbb063b51669d710d384b32616.tar.gz) {};
-   loco = callPackage (fetchTarball https://github.com/totten/loco/archive/v0.2.1.tar.gz) {};
+   loco = callPackage (fetchTarball https://github.com/totten/loco/archive/v0.2.2.tar.gz) {};
    launcher = import ./launcher;
 }
