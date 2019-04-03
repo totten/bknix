3874504d14936c2f75cf355d0ae8e4735a8b6f3d
Tim Otten <totten@civicrm.org>
2019-04-03 07:48:29 +0000

    restart-ci.sh - Autodetect local svcs. Accept optional subcmd

--- bin/restart-ci.sh
+++ bin/restart-ci.sh
@@ -1,13 +1,52 @@
 #!/bin/bash
+GUARD=
 
 set -e
 
-echo "Stopping all services"
-systemctl stop bknix-{dfl,min,max}{,-mysqld} mnt-mysql-jenkins.mount
+function get_svcs() {
+  for svc in bknix{,-publisher}-{dfl,min,max,edge}{,-mysqld} ; do
+    if [ -f "/etc/systemd/system/$svc.service" ]; then
+      echo $svc
+    fi
+  done
+}
+function get_ramdisks() {
+  for svc in mnt-mysql-{jenkins,publisher}.mount ; do
+    if [ -f "/etc/systemd/system/$svc" ]; then
+      echo $svc
+    fi
+  done
+}
 
-echo "Waiting"
-# Don't know if this is actually needed, but it's improved reliability in the past.
-sleep 5
+function do_stop() {
+  echo "Stopping all services"
+  $GUARD systemctl stop $(get_svcs)
+  $GUARD sleep 3 # Don't know if this is actually needed, but it's improved reliability in the past.
+  echo "Stopping all ramdisks"
+  $GUARD systemctl stop $(get_ramdisks)
+}
 
-echo "Starting all services"
-systemctl start mnt-mysql-jenkins.mount bknix-{dfl,min,max}{,-mysqld}
+
+function do_start() {
+  echo "Starting all ramdisks"
+  $GUARD systemctl start $(get_ramdisks)
+  $GUARD sleep 3 # Don't know if this is actually needed, but it's improved reliability in the past.
+  echo "Starting all services"
+  $GUARD systemctl start $(get_svcs)
+}
+
+case "$1" in
+  stop) do_stop ;;
+  start) do_start ;;
+  status) systemctl status $(get_svcs) $(get_ramdisks) ;;
+  ""|restart)
+    do_stop
+    echo "Waiting"
+    $GUARD sleep 3 # Don't know if this is actually needed, but it's improved reliability in the past.
+    do_start
+    ;;
+  *)
+    echo "unrecognized action: $1"
+    exit 1
+    ;;
+esac
