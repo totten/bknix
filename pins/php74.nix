970cc5cbc9755e6de21b183524a5fdf43403054e
Tim Otten <totten@civicrm.org>
2020-03-07 12:07:13 -0800

    Revert "pins/php74.nix - Bump up to 7.4.2"
    This reverts commit dc2a71325b6db63581358b7133b1533444ddff96.

--- pins/php74.nix
+++ pins/php74.nix
@@ -1,5 +1,5 @@
-## Track latest iteration of 20.03.
-# fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz
+## Track latest iteration of 19.09.
+# fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz
 
-## Use a specific iteration between 19.09 and 20.03
-fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/5d058471a9b878ce4849910810ce8a4fac66c283.tar.gz
+## Use a specific 19.09 iteration.
+fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/9e4716b1f57bc038f38fe6e9b7c3520299330fa6.tar.gz
