c330daf2cde503ce137e41ef79a8d17acdf938b4
Tim Otten <totten@civicrm.org>
2020-04-30 23:17:09 -0700

    pkgs/transifexClient - Update 0.13.5 => 0.13.9
    This also updates the base environment from 18.09 to 19.09.

--- pkgs/transifexClient/transifexClient.nix
+++ pkgs/transifexClient/transifexClient.nix
@@ -1,17 +1,26 @@
-{ lib, python, fetchFromGitHub, slugify, requests }:
+{ lib, python, fetchFromGitHub, python-slugify, six, requests }:
 with python.pkgs;
 buildPythonApplication rec {
   pname = "transifex-client";
-  version = "0.13.5";
+  version = "0.13.9";
+
+  propagatedBuildInputs = [
+    urllib3 requests python-slugify six setuptools
+  ];
 
   src = fetchPypi {
     inherit pname version;
-    sha256 = "00igk35nyzqp1slj7lbhiv4lc42k87ix43ipx2zcrsjf6xxv6l7v";
+    sha256 = "0lgd77vrddvyn8afkxr7a7hblmp4k5sr0i9i1032xdih2bipdd9f";
   };
+
+  prePatch = ''
+    substituteInPlace requirements.txt --replace "urllib3<1.24" "urllib3>=1.24" \
+      --replace "six==1.11.0" "six>=1.11.0" \
+      --replace "python-slugify<2.0.0" "python-slugify>2.0.0"
+  '';
 
   doCheck = false;
 
-  propagatedBuildInputs = [ urllib3 slugify requests ];
 
   meta = {
     homepage = https://github.com/transifex/transifex-client;
