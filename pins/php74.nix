0d09cd2f9e3a93f05400ca401950497c62c78798
Tim Otten <totten@civicrm.org>
2020-06-09 17:03:11 -0700

    php74 - Update to 7.4.6

--- pins/php74.nix
+++
@@ -1,5 +0,0 @@
-## Track latest iteration of 20.03.
-# fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz
-
-## Use a specific iteration between 19.09 and 20.03
-fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/5d058471a9b878ce4849910810ce8a4fac66c283.tar.gz
