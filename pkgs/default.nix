90283984d5053f5196db115492072b2cd46a58c5
Tim Otten <totten@civicrm.org>
2020-06-17 00:50:17 -0700

    Update loco to v0.4.2

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -25,7 +25,7 @@ in rec {
    php74 = import ./php74/default.nix;
    transifexClient = import ./transifexClient/default.nix;
    ramdisk = callPackage (fetchTarball https://github.com/totten/ramdisk/archive/v0.1.1.tar.gz) {};
-   loco = callPackage (fetchTarball https://github.com/totten/loco/archive/v0.4.1.tar.gz) {};
+   loco = callPackage (fetchTarball https://github.com/totten/loco/archive/v0.4.2.tar.gz) {};
    # loco = callPackage /PATH/TO/src/loco {};
    launcher = import ./launcher;
 }
