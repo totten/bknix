7352ea3f249838d5d7aab72b3a0be7b68cefaf5c
Tim Otten <totten@civicrm.org>
2020-06-17 00:45:59 -0700

    examples/gcloud-bknix-ci - Fix computation of HTTPD_DOMAIN
    The technique of using `dynamic_env_{USER}.sh` only worked for *running* the
    service (i.e.  `loco run`).  It didn't work with other commands, like
    `loco init`, `loco env`, or `loco sh`.
    
    This technique should work with all commands.

--- examples/gcloud-bknix-ci/dynamic_env_jenkins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-## We often run gcloud VMs with preemptible instances and ephemeral IPs.
-## The `*.nip.io` domain automatically maps between DNS and IPs.
-## Use HTTP URLs akin to "http://123.123.123.123.nip.io/"
-
-HTTPD_DOMAIN=$(curl 'http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip' -H "Metadata-Flavor: Google").nip.io
